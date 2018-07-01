826c6c7dc208f8b5d454fd7a92ae31f57362c749
tigerbunny02 <tigerbunny@rambler.ru>
2018-07-01 09:49:43 +0600

    first commit

---
+++ src/second.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : second.cpp
+// Author      : pirozhok
+// Version     :
+// Copyright   : AAAAAAAAAAAAAAA!!!!!!!!
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Hello World!!!" << endl; // prints Hello World!!!
+	return 0;
+}
